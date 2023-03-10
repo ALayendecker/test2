efc7021226a238b8aacea8d48eaca3d6c3cf2da6
Alayendecker <andrewlayendecker@gmail.com>
2023-03-10 14:01:57 -0600

    testing3

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,32 +1,28 @@
 import React from "react";
 import projects from "../projects.json";
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
 
 function Portfolio(props) {
   return (
-    <Card style={{ width: '18rem' }}>
-
+    <Card style={{ width: "18rem" }}>
       {projects.map((item, index) => {
         return (
           <div key={item.id}>
-      <Card.Img variant="top" src={process.env.PUBLIC_URL + item.image} />
-      <Card.Body>
+            <Card.Img variant="top" src={process.env.PUBLIC_URL + item.image} />
+            <Card.Body>
               <Card.Title>{item.title}</Card.Title>
-         <Card.Text>
-         {item.description}
-        </Card.Text>
-          </Card.Body>
+              <Card.Text>{item.description}</Card.Text>
+            </Card.Body>
 
-        <Card.Body>
-        <Card.Link href="{item.link}">Website</Card.Link>
-        <Card.Link href="{item.repo}">GitHub</Card.Link>
-        </Card.Body>
-        </div>
-          );
+            <Card.Body>
+              <Card.Link href="{item.link}">Website</Card.Link>
+              <Card.Link href="{item.repo}">GitHub</Card.Link>
+            </Card.Body>
+          </div>
+        );
       })}
     </Card>
-    );
-    }
+  );
+}
 
-   
 export default Portfolio;
